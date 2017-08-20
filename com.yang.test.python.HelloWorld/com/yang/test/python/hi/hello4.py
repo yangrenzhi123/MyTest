e1329868e0b4fd3f33eced482070a0cb96c1fa56
yangrenzhi123 <yangrenzhi123@yangrenzhi123.com>
2017-08-20 21:40:23 +0800

    python

---
+++ com.yang.test.python.HelloWorld/com/yang/test/python/hi/hello4.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+print np.percentile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 50)
+a = np.array([[1, 2, 3], [4, 5]])
+b = np.mean([1, 1, 1])
+c = np.std([1, 4])
+print a
+print b
+print c
