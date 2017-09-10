3e53a4e7da4708b5adb6dce1840b6d3afbe54e1f
yangrenzhi123 <yangrenzhi123@yangrenzhi123.com>
2017-09-10 23:15:15 +0800

    1

--- com.yang.test.python.HelloWorld/com/yang/test/python/hi/example.py
+++ com.yang.test.python.HelloWorld/com/yang/test/python/hi/example.py
@@ -1,10 +1,19 @@
 # -*- coding: gbk -*-
 import numpy as np
 
+# print np.array([[1, 2, 3], [4, 5]])
 # 先排序，再取50%位置的数字，得出这些数字的平均值。
-print np.percentile([1, 6, 7, 2, 9, 3, 4, 5, 8], 50)
-print np.array([[1, 2, 3], [4, 5]])
+# print np.percentile([1, 6, 7, 2, 9, 3, 4, 5, 8], 50)
 # 求平均数
-print np.mean([1, 1, 4])
+# print np.mean([1, 1, 4])
 # 求总体标准差
-print np.std([1, 4])
+# print np.std([1, 4])
+
+print [1, 2].ilo
+
+# row = [1, 2, 3, 4, 7]
+# print row[-1]
+# row.pop()
+# print row
+# row.pop()
+# print row
