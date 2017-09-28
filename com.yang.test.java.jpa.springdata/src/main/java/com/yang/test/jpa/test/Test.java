b1d9c626300d1c85d61bb1c2046fe02e69746fe7
unknown <yangrenzhi@360ser.com>
2017-09-28 15:27:09 +0800

    1

--- com.yang.test.java.jpa.springdata/src/main/java/com/yang/test/jpa/test/Test.java
+++ com.yang.test.java.jpa.springdata/src/main/java/com/yang/test/jpa/test/Test.java
@@ -1,50 +1,28 @@
 package com.yang.test.jpa.test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.yang.test.jpa.dao.SpidGetter;
 import com.yang.test.jpa.dao.TaskDao;
-import com.yang.test.jpa.domain.T;
 
-public class Test {
+public class Test extends BaseJunit4Test {
+
+	@Autowired
+	TaskDao taskDao;
+
+	@org.junit.Test
+	@Transactional
+	@Rollback(true)
+	public void test() {
+		System.out.println(1);
+	}
 
 	public static void main(String[] args) {
 		ApplicationContext ac = new FileSystemXmlApplicationContext("classpath:root-context.xml");
-//		SpidGetter spidGetter = (SpidGetter) ac.getBean("spidGetter");
-//		Short spid = spidGetter.getSpid();
-//		System.out.println(spid);
-
 		TaskDao b = (TaskDao) ac.getBean("taskDao");
 		b.delete(1);
-		
-
-//		Sort sort = new Sort(Direction.DESC, "id");
-//		Pageable pageable = new PageRequest(0, 1, sort);
-//		
-//		Page<T> t = (Page<T>) b.findAll(new Specification<T>() {
-//
-//			public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-//				List<Predicate> list = new ArrayList<Predicate>();
-//				list.add(cb.equal(root.get("id").as(Integer.class), 1));
-//
-//				Predicate[] p = new Predicate[list.size()];
-//				return cb.and(list.toArray(p));
-//			}
-//		}, pageable);
 	}
 }
