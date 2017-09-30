b81799f137ad21aad44c4f9c30469c14b2001811
yangrenzhi123 <yangrenzhi123@yangrenzhi123.com>
2017-09-30 23:35:35 +0800

    1

---
+++ com.yang.test.java.pure/src/SpeedTest.java
@@ -0,0 +1,29 @@
+public class SpeedTest {
+
+	public static void main(String[] args) {
+		try {
+
+			for (int i = 0; i < 1; i++) {
+				new Thread(new Runnable() {
+
+					public void run() {
+						test28();
+					}
+				}).start();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void test28() {
+		long l = System.currentTimeMillis();
+
+		long max = 100000000000L;
+		System.out.println(max);
+		for (long i = 0; i < max; i++) {
+		}
+
+		System.out.println(System.currentTimeMillis() - l);
+	}
+}
