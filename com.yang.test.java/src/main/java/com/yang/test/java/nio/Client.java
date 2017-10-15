067f524c06dae634404b31488001929d19b78e82
yangrenzhi123 <yangrenzhi123@163.com>
2017-10-15 09:34:43 +0800

    1

--- com.yang.test.java/src/main/java/com/yang/test/java/nio/Client.java
+++ com.yang.test.java/src/main/java/com/yang/test/java/nio/Client.java
@@ -6,41 +6,23 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.net.InetAddress;
 
-/**
- * TCP/IP的NIO非阻塞方式 客户端
- */
 public class Client {
-
-	// 创建缓冲区
-	private ByteBuffer buffer = ByteBuffer.allocate(512);
-	// 访问服务器
 
 	public void query(String host, int port) throws IOException {
 		InetSocketAddress address = new InetSocketAddress(InetAddress.getByName(host), port);
-		SocketChannel socket = null;
-		byte[] bytes = new byte[512];
+		SocketChannel socket = SocketChannel.open();
+		socket.connect(address);
+		int i = 0;
+		ByteBuffer buffer = ByteBuffer.allocate(1024000);
 		while (true) {
-			try {
-				System.in.read(bytes);
-				socket = SocketChannel.open();
-				socket.connect(address);
-				buffer.clear();
-				buffer.put(bytes);
-				buffer.flip();
-				socket.write(buffer);
-				buffer.clear();
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				if (socket != null) {
-					socket.close();
-				}
-			}
+			buffer.clear();
+			buffer.put((i++ + "\n").getBytes());
+			buffer.flip();
+			socket.write(buffer);
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
-		new Client().query("localhost", 8099);
-
+		new Client().query("192.168.1.102", 8099);
 	}
 }
