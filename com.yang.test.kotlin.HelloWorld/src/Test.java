530d5ab4c7bf8d498745cb27a707a617039bb61d
yangrenzhi123 <yangrenzhi123@yangrenzhi123.com>
2017-10-21 10:26:41 +0800

    1

---
+++ com.yang.test.kotlin.HelloWorld/src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+
+    public static void main(String[] args) {
+        final KtTest ktTest = new KtTest();
+        System.out.print(ktTest.sum(1, 2));
+    }
+}
---
+++ com.yang.test.kotlin.HelloWorld/src/app.kt
@@ -0,0 +1,10 @@
+class KtTest{
+
+    fun main(args: Array<String>){
+        print(sum(1, 2))
+    }
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+}
