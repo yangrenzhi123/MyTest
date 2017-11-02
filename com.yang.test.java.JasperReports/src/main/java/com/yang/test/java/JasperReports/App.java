53c160b479d2b8fa0641f8ada420c8a8aa7074e5
unknown <yangrenzhi@360ser.com>
2017-11-02 09:20:54 +0800

    1

--- com.yang.test.java.JasperReports/src/main/java/com/yang/test/java/JasperReports/App.java
+++ com.yang.test.java.JasperReports/src/main/java/com/yang/test/java/JasperReports/App.java
@@ -1,5 +1,7 @@
 package com.yang.test.java.JasperReports;
-
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.HashMap;
@@ -11,26 +13,33 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JsonDataSource;
 
 public class App {
 
 	public static void main(String[] args) throws JRException, Exception {
-		JasperReport r = JasperCompileManager.compileReport(
-				"C:/Users/yangrenzhi/Documents/GitHub/MyTest/com.yang.test.java.JasperReports/src/main/java/com/yang/test/java/JasperReports/default_cover.jrxml");
-
+		JasperReport r = JasperCompileManager.compileReport("C:/Users/yangrenzhi/JaspersoftWorkspace/MyReports/Blank_A4.jrxml");
+		
+		
+		
 		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("tenantCode", 111);
-		params.put("seqNumber", "13654");
-		params.put("logoPath", "/upload/reportTemplate/20170704201606461/logo.jpg");
-		params.put("ip", "http://192.168.6.240:8080");
-//		params.put("id", 15);
+//		params.put("id", "15");
+		
+
+		String json = "{\"a\":{\"b\":[\"1\", \"2\", \"3\", \"4\"]}}";
+		InputStream is = new ByteArrayInputStream(json.getBytes());
+		params.put("JSON_INPUT_STREAM", is);
 
 		Class.forName("net.sourceforge.jtds.jdbc.Driver");
-//		Connection connection = DriverManager.getConnection("jdbc:jtds:sqlserver://127.0.0.1:1433/Test", "sa", "1qazxcvbnm,./");
-		Connection connection = DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.6.225:1433/zhengyuandb1", "hb6debug", "hb6debug");
+		Connection connection = DriverManager.getConnection("jdbc:jtds:sqlserver://127.0.0.1:1433/Test", "sa", "1qazxcvbnm,./");
+//		Connection connection = DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.6.225:1433/zhengyuandb1", "hb6debug", "hb6debug");
+//		JasperPrint p2 = JasperFillManager.fillReport(r, params, connection);
 
-		JasperPrint p = JasperFillManager.fillReport(r, params, connection);
-
-		JasperExportManager.exportReportToPdfFile(p, "c:/1.pdf");
+//		JasperPrint p = JasperFillManager.fillReport(r, params, new JREmptyDataSource());
+//		JasperPrint p3 = JasperFillManager.fillReport(r, params, new JsonDataSource(new File("C:/Users/yangrenzhi/Desktop/test2.json")));
+		JasperPrint p4 = JasperFillManager.fillReport("C:\\Users\\yangrenzhi\\JaspersoftWorkspace\\MyReports\\Blank_A4.jasper", 
+				params, new JsonDataSource(is));
+		//JasperPrint p4 = JasperFillManager.fillReport(r, params);
+		JasperExportManager.exportReportToPdfFile(p4, "c:/1.pdf");
 	}
 }
