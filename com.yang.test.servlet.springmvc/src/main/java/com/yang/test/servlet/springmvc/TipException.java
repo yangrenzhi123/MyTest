b8aa3f0744287f5fd7de849956202a77dd759d0f
yangrenzhi123 <yangrenzhi123@163.com>
2017-11-22 11:34:55 +0800

    1

--- com.yang.test.servlet.springmvc/src/main/java/com/yang/test/servlet/springmvc/TipException.java
+++ com.yang.test.servlet.springmvc/src/main/java/com/yang/test/servlet/springmvc/TipException.java
@@ -2,9 +2,5 @@ package com.yang.test.servlet.springmvc;
 
 public class TipException extends RuntimeException {
 
-//	public TipException(String message){
-//		super(message);
-//	}
-	
 	private static final long serialVersionUID = 7827444065800695635L;
 }
