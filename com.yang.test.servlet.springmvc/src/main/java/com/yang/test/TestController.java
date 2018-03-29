fd1b27b433ce2c801e24845d1e2155c500142ace
yangrenzhi123 <yangrenzhi123@163.com>
2018-03-29 18:21:42 +0800

    1

--- com.yang.test.servlet.springmvc/src/main/java/com/yang/test/TestController.java
+++ com.yang.test.servlet.springmvc/src/main/java/com/yang/test/TestController.java
@@ -1,22 +1,17 @@
 package com.yang.test;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.yang.test.jpa.dao.TaskDao;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 
-	@Autowired
-	TaskDao taskDao;
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String iframeTest2() {
-		taskDao.findAll();
-		
-		return "/iframeTest2";
+	@ResponseBody
+	@RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+	public String iframeTest2(@RequestParam("accountCode") String cc) {
+		return "123";
 	}
 }
--- com.yang.test.servlet.springmvc/src/main/java/com/yang/test/jpa/dao/TaskDao.java
+++
@@ -1,9 +0,0 @@
-package com.yang.test.jpa.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import com.yang.test.jpa.domain.T;
-
-public interface TaskDao extends JpaRepository<T, Integer>, JpaSpecificationExecutor<T> {
-}
