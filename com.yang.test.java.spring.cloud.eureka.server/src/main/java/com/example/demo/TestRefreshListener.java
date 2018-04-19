cbd9e4fe2a53bac51143f177ed23276d9eccd2ba
yangrenzhi123 <yangrenzhi123@163.com>
2018-04-19 18:00:13 +0800

    1

--- com.yang.test.java.spring.cloud.eureka.server/src/main/java/com/example/demo/TestRefreshListener.java
+++ com.yang.test.java.spring.cloud.eureka.server/src/main/java/com/example/demo/TestRefreshListener.java
@@ -1,11 +1,20 @@
 package com.example.demo;
 
 import org.springframework.cloud.bus.event.RefreshListener;
+import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
 import org.springframework.cloud.context.refresh.ContextRefresher;
 
 public class TestRefreshListener extends RefreshListener {
 
 	public TestRefreshListener(ContextRefresher contextRefresher) {
 		super(contextRefresher);
+	}
+
+	@Override
+	public void onApplicationEvent(RefreshRemoteApplicationEvent event) {
+		System.out.println(1);
+		
+		super.onApplicationEvent(event);
+		System.out.println(2);
 	}
 }
