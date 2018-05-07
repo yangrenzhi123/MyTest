8ee04d6a9a5fd9f8fa14e23e9dda7700b26dd749
yangrenzhi123 <yangrenzhi123@163.com>
2018-05-07 19:17:32 +0800

    1

--- com.yang.test.java.windows.service/src/Test.java
+++ com.yang.test.java.windows.service/src/Test.java
@@ -1,8 +1,32 @@
-public class Test {
+import org.tanukisoftware.wrapper.WrapperListener;
+import org.tanukisoftware.wrapper.WrapperManager;
+
+public class Test implements WrapperListener {
 
 	public static void main(String[] args) throws InterruptedException {
-		while (true) {
-			Thread.sleep(1000 * 5);
+
+		WrapperManager.start(new Test(), args);
+		
+		int i=0;
+		while(true){
+			System.out.println(i++);
+			Thread.sleep(1000);
 		}
+	}
+
+	@Override
+	public void controlEvent(int arg0) {
+	}
+
+	@Override
+	public Integer start(String[] arg0) {
+		System.out.println("start");
+		return null;
+	}
+
+	@Override
+	public int stop(int arg0) {
+		System.out.println("stop");
+		return 0;
 	}
 }
