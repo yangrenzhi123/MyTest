35bf31d6caa4b9fb950314da97a49ec581a48e06
yangrenzhi123 <yangrenzhi123@163.com>
2018-07-19 10:34:30 +0800

    1

---
+++ SocketClient/src/com/yang/test/java/socket/simple/SimpleClient.java
@@ -0,0 +1,35 @@
+package com.yang.test.java.socket.simple;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+@SuppressWarnings("resource")
+public class SimpleClient {
+
+	public static Socket request = null;
+
+	public static void main(String[] args) throws InterruptedException, UnknownHostException, IOException {
+
+		Socket request = new Socket("127.0.0.1", 8099);
+		OutputStream os = request.getOutputStream();
+		InputStream is = request.getInputStream();
+		
+		while(true){
+			byte[] a = new byte[100];
+			System.in.read(a);
+			byte[] b = new byte[1];
+			b[0] = a[0];
+			os.write(b);
+			
+			System.out.println(1);
+			
+
+			byte[] c = new byte[1];
+			is.read(c);
+			System.out.println(c[0]);
+		}
+	}
+}
---
+++ SocketClient/src/com/yang/test/java/socket/simple/SimpleServer.java
@@ -0,0 +1,40 @@
+package com.yang.test.java.socket.simple;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@SuppressWarnings("resource")
+public class SimpleServer {
+
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket serverSocket = new ServerSocket(8099);
+		while (true) {
+			final Socket socket = serverSocket.accept();
+			new Thread(new Runnable() {
+				public void run() {
+					try {
+						byte [] b = new byte[1];
+						byte [] c = new byte[]{2};
+						
+						InputStream is = socket.getInputStream();
+						OutputStream os = socket.getOutputStream();
+						
+						while(true){
+							is.read(b);
+							
+							System.out.println(b[0]);
+							
+							os.write(c);
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}).start();
+		}
+	}
+}
