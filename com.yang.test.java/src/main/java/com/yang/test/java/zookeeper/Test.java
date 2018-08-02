93a6bd8e8715f459d5201780a73405449fd9f0d1
yangrenzhi123 <yangrenzhi123@163.com>
2018-08-02 11:28:17 +0800

    1

--- com.yang.test.java/src/main/java/com/yang/test/java/zookeeper/Test.java
+++ com.yang.test.java/src/main/java/com/yang/test/java/zookeeper/Test.java
@@ -14,25 +14,21 @@ import org.apache.zookeeper.data.Stat;
 public class Test {
 
 	public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
-		ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 12000, new TestWatcher());
-		String registryPath = "/com.xk.msa.registry";
+		ZooKeeper zk = new ZooKeeper("127.0.0.1:2001", 12000, new TestWatcher());
+		String registryPath = "/test";
 
 		if(zk.exists(registryPath, false) == null){
 			zk.create(registryPath, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-		}else{
-			System.out.println("exsit");
 		}
-		
 
 		//创建节点
-		zk.create("/com.xk.msa.registry/address-", "192.168.5.152:8080".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-		zk.create("/com.xk.msa.registry/address-", "192.168.5.152:8080".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+		zk.create("/test/address-", "192.168.5.152:8080".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+		zk.create("/test/address-", "192.168.5.152:8080".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
 		
-		List<String> l = zk.getChildren("/com.xk.msa.registry", false);
+		List<String> l = zk.getChildren("/test", false);
 		
-		byte[] bs = zk.getData("/com.xk.msa.registry/" + l.get(0),true,new Stat());
+		byte[] bs = zk.getData("/test/" + l.get(0),true,new Stat());
 		System.out.println(new String(bs));
-		System.out.println();
 		Thread.sleep(1000000000);
 	}
 }
