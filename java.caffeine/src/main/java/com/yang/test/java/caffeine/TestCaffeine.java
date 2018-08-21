9e24fea1dcc454381895f36709dce6ff6e42ec06
yangrenzhi123 <yangrenzhi123@163.com>
2018-08-21 16:37:44 +0800

    1

---
+++ java.caffeine/src/main/java/com/yang/test/java/caffeine/TestCaffeine.java
@@ -0,0 +1,62 @@
+package com.yang.test.java.caffeine;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+
+public class TestCaffeine {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		t3();
+	}
+	
+	//手动
+	public static void t0() {
+		Cache<Integer, Object> a = Caffeine.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS).maximumSize(100).build();
+		
+		a.put(1, "123");
+
+		System.out.println(a.getIfPresent(1));
+	}
+
+	//手动
+	public static void t1() {
+		Cache<Integer, Object> a = Caffeine.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS).maximumSize(100).build();
+
+		System.out.println(a.get(1, k -> "123"));
+	}
+
+	//同步加载
+	public static void t2() {
+		LoadingCache<Integer, Object> a = Caffeine.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS).maximumSize(100).build(k -> "i got " + k);
+
+		System.out.println(a.get(1));
+		
+		a.getAll(Arrays.asList(2, 3, 4));
+	}
+
+	//异步加载
+	public static void t3() throws InterruptedException, ExecutionException {
+		AsyncLoadingCache<Integer, Object> a = Caffeine.newBuilder().maximumSize(100).expireAfterWrite(1, TimeUnit.MINUTES).buildAsync(k -> "i got " + k);
+		
+		
+		CompletableFuture<Void> b = a.get(1).thenAccept(o -> {
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println(o);
+		});
+		
+		
+		System.out.println(b.get());
+	}
+}
