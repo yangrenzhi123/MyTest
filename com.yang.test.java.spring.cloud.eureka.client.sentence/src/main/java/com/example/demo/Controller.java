36762789440548ee6b11b7c0387bcde02529f44f
yangrenzhi123 <yangrenzhi123@163.com>
2018-08-24 09:17:48 +0800

    1

--- com.yang.test.java.spring.cloud.eureka.client.sentence/src/main/java/com/example/demo/Controller.java
+++ com.yang.test.java.spring.cloud.eureka.client.sentence/src/main/java/com/example/demo/Controller.java
@@ -1,17 +1,13 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-	@Value("${words}")
-	String words;
-
 	@RequestMapping("/")
 	public String getWord() {
-		return words;
+		return "1";
 	}
 }
