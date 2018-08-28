4dcce24c552060c15b191ae7cf339d1c9c6a0653
yangrenzhi123 <yangrenzhi123@163.com>
2018-08-28 17:25:28 +0800

    1

--- com.yang.test.java.jedis/src/main/java/com/yang/test/java/jedis/App.java
+++ com.yang.test.java.jedis/src/main/java/com/yang/test/java/jedis/App.java
@@ -4,9 +4,9 @@ import redis.clients.jedis.Jedis;
 
 public class App {
 	public static void main(String[] args) {
-		Jedis j = new Jedis("192.168.19.147");
+		Jedis j = new Jedis("172.28.51.33");
 
-		//j.set("testKey", "YangRenZhi");
+		j.set("testKey", "YangRenZhi");
 		System.out.println(j.get("testKey"));
 		
 		j.close();
