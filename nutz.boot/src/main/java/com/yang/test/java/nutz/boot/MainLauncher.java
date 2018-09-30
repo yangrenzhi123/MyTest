dbe7d9aae18e7088d95c4d1f7ce91b0621773748
yangrenzhi123 <yangrenzhi123@163.com>
2018-09-30 13:58:35 +0800

    1

---
+++ nutz.boot/src/main/java/com/yang/test/java/nutz/boot/MainLauncher.java
@@ -0,0 +1,19 @@
+package com.yang.test.java.nutz.boot;
+
+import org.nutz.boot.NbApp;
+import org.nutz.ioc.loader.annotation.*;
+import org.nutz.mvc.annotation.*;
+
+@IocBean
+public class MainLauncher {
+
+    @Ok("raw")
+    @At("/time/now")
+    public long now() {
+        return System.currentTimeMillis();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new NbApp(MainLauncher.class).run();
+    }
+}
