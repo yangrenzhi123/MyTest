edf0830cb357e519039fefb439602f4711714856
yangrenzhi123 <yangrenzhi123@163.com>
2018-10-19 15:14:37 +0800

    1

--- com.yang.test.java.spring.cloud/com.yang.test.java.spring.cloud.eureka.client.sentence/src/main/java/com/example/demo/Controller.java
+++ com.yang.test.java.spring.cloud/com.yang.test.java.spring.cloud.eureka.client.sentence/src/main/java/com/example/demo/Controller.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+	@Value("${words}")
+	private String words;
+
 	@RequestMapping("/")
 	public String getWord() throws InterruptedException {
-		Thread.sleep(20000L);
+		System.out.println("words:"+words);
 		
 		return "1";
 	}
