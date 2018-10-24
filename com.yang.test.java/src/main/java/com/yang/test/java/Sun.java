d0c305376cb869788e590237b58d6deea993e8fe
yangrenzhi123 <yangrenzhi123@163.com>
2018-10-24 18:37:11 +0800

    1

--- com.yang.test.java/src/main/java/com/yang/test/java/Sun.java
+++ com.yang.test.java/src/main/java/com/yang/test/java/Sun.java
@@ -2,7 +2,22 @@ package com.yang.test.java;
 
 public class Sun extends Father {
 
+	public static void main(String[] args) {
+		new Sun().service();
+	}
+	
 	public void doService() {
 		System.out.println("Sun.doService");
+	}
+}
+
+class Father {
+
+	public void service() {
+		doService();
+	}
+	
+	public void doService(){
+		System.out.println("Father.doService");
 	}
 }
--- springboot.Scheduled/src/main/java/com/yang/test/java/springboot/Scheduled/MyTask.java
+++ springboot.Scheduled/src/main/java/com/yang/test/java/springboot/Scheduled/MyTask.java
@@ -5,7 +5,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyTask {
-    @Scheduled(cron = "0/10 * * * * *")
+
+	
+	
+	
+	//	秒（0~59）
+	//	分钟（0~59）
+	//	小时（0~23）
+	//	天（月）（0~31，但是你需要考虑你月的天数）
+	//	月（0~11）
+	//	天（星期）（1~7 1=SUN 或 SUN，MON，TUE，WED，THU，FRI，SAT）
+	//	年份（1970－2099）
+    @Scheduled(cron = "0/10 * * * * * *")
     public void work() {
         System.out.println(1);
     }
