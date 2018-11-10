945f2f5cfd84c5b87d5103a6a4c600b177f80527
yangrenzhi123 <yangrenzhi123@163.com>
2018-11-10 13:07:47 +0800

    1

---
+++ lombok/src/main/java/com/yang/test/java/lombok/Lmbk.java
@@ -0,0 +1,9 @@
+package com.yang.test.java.lombok;
+
+import lombok.Data;
+
+@Data
+public class Lmbk {
+
+	private String id;
+}
---
+++ lombok/src/main/java/com/yang/test/java/lombok/Test.java
@@ -0,0 +1,7 @@
+package com.yang.test.java.lombok;
+
+public class Test {
+	public static void main(String[] args) {
+		new Lmbk().getId();
+	}
+}
