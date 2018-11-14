2cb237abfd2419c00728980aef4089b65f2d17fc
yangrenzhi123 <yangrenzhi123@163.com>
2018-11-14 10:23:39 +0800

    1

---
+++ junit.surefire2/src/main/java/com/yang/test/java/junit/App.java
@@ -0,0 +1,12 @@
+package com.yang.test.java.junit;
+
+import org.junit.Test;
+
+/* mvn test surefire-report:report **/
+public class App {
+
+	@Test
+	public void TestMth3() {
+		System.out.println(1);
+	}
+}
