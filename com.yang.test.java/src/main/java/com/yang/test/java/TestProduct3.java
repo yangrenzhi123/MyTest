93a08adaa575f89673eb519d6a6329f4eec104b3
yangrenzhi123 <yangrenzhi123@163.com>
2018-11-18 00:30:37 +0800

    1

--- com.yang.test.java/src/main/java/com/yang/test/java/TestProduct3.java
+++ com.yang.test.java/src/main/java/com/yang/test/java/TestProduct3.java
@@ -25,7 +25,7 @@ public class TestProduct3 {
 	static String logPath = "C:/1.txt";
 	static RandomAccessFile f;
 	static List<Runnable> rl;
-	static int count = 1000;
+	static int count = 9000;
 	static CountDownLatch counttime;
 	static long totalDistance;
 	
@@ -36,6 +36,7 @@ public class TestProduct3 {
 	static Integer countException;
 	
 	static long totalTime;
+	static String ip="127.0.0.1";
 	
 	public static void main(String[] args) throws InterruptedException, IOException {
 		File log = new File(logPath);
@@ -49,7 +50,7 @@ public class TestProduct3 {
 		
 		RequestConfig config = RequestConfig.custom().setConnectTimeout(300000).setConnectionRequestTimeout(300000).setSocketTimeout(300000).build();  
 
-		final HttpGet p3 = new HttpGet("http://192.168.10.240:8080/");
+		final HttpGet p3 = new HttpGet("http://"+ip+":8080/");
 		p3.setHeader("Connection", "Keep-Alive");
 		final List<CloseableHttpClient> hcl = new ArrayList<>();
 		for(int i = 0; i < count; i++) {
@@ -118,6 +119,8 @@ public class TestProduct3 {
 	static void test(int index) throws InterruptedException, IOException {
 		counttime = new CountDownLatch(count);
 		countZhengchang = 0;
+		count500 = 0;
+		count502 = 0;
 		countTimeout = 0;
 		countException = 0;
 
@@ -139,7 +142,7 @@ public class TestProduct3 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream(), "GBK"));
 		String line;
 		while ((line = br.readLine()) != null) {
-			if(line.contains("ESTABLISHED") && line.contains("192.168.10.240")) {
+			if(line.contains("ESTABLISHED") && line.contains(ip)) {
 				count++;
 			}
 		}
@@ -151,11 +154,11 @@ public class TestProduct3 {
 		br.close();
 		
 		if(index == 0) {
-			System.out.println("第一次耗时："+distance+"，平均耗时：不计入平均时，正常数："+countZhengchang+"，超时数："+countTimeout+"，异常数："+countException+"，连接数："+count+"，总耗时："+(System.currentTimeMillis()-totalTime));
+			System.out.println("第一次耗时："+distance+"，平均耗时：不计入平均时，正常数："+countZhengchang+"，500："+count500+"，502："+count502+"，超时数："+countTimeout+"，异常数："+countException+"，连接数："+count+"，总耗时："+(System.currentTimeMillis()-totalTime));
 			return ;
 		}
 		totalDistance = totalDistance + distance;
 		long avg = totalDistance / index;
-		System.out.println("本次耗时："+distance+"，平均耗时："+avg+",正常数："+countZhengchang+"，超时数："+countTimeout+"，异常数："+countException+"，连接数："+count+"，总耗时："+(System.currentTimeMillis()-totalTime));
+		System.out.println("本次耗时："+distance+"，平均耗时："+avg+",正常数："+countZhengchang+"，500："+count500+"，502："+count502+"，超时数："+countTimeout+"，异常数："+countException+"，连接数："+count+"，总耗时："+(System.currentTimeMillis()-totalTime));
 	}
 }
