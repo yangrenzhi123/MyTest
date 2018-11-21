aced56dc48c43fef3b02d66fc875c9bbc70aab14
yangrenzhi123 <yangrenzhi123@163.com>
2018-11-21 19:03:25 +0800

    1

--- springboot/src/main/java/com/example/demo/DemoApplication.java
+++ springboot/src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/** 注意Controller包在同包或者子包下 */
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@RestController
+class HelloController {
+
+	@GetMapping("/")
+	public String hello2() {
+		return "123456";
 	}
 }
