3c106c013a7e21242a47cfb856a518d473b6e261
yangrenzhi123 <yangrenzhi123@163.com>
2018-12-05 11:50:28 +0800

    1

--- com.yang.test.java/src/main/java/Kafka2.java
+++ com.yang.test.java/src/main/java/Kafka2.java
@@ -17,9 +17,16 @@ public class Kafka2 {
 		//properties.put("zookeeper.connect", "172.28.51.33:2181");// 声明zk
 		//properties.put("metadata.broker.list", "172.28.51.33:9092,172.28.51.33:9093,172.28.51.33:9094");// 声明kafka
 
-		properties.put("zookeeper.connect", "192.168.30.120:2181,192.168.30.121:2181,192.168.10.10:2181");
-		properties.put("metadata.broker.list", "192.168.30.151:9092,192.168.30.152:9092,192.168.30.153:9092");
+//		properties.put("zookeeper.connect", "192.168.30.120:2181,192.168.30.121:2181,192.168.10.10:2181");
+//		properties.put("metadata.broker.list", "192.168.30.151:9092,192.168.30.152:9092,192.168.30.153:9092");
 		
+
+//		properties.put("zookeeper.connect", "192.168.8.157:2181");
+//		properties.put("metadata.broker.list", "192.168.8.157:9092");
+		
+		
+		properties.put("zookeeper.connect", "192.168.30.120:2181,192.168.30.121:2181,192.168.10.10:2181");
+		properties.put("metadata.broker.list", "192.168.30.120:9092,192.168.8.70:9092");
 		
 
 		//properties.put("zookeeper.connect", "192.168.10.248:2181");
@@ -29,8 +36,9 @@ public class Kafka2 {
 		//properties.put("zookeeper.connect", "192.168.30.60:2181,192.168.30.61:2181,192.168.30.62:2181");
 		//properties.put("metadata.broker.list", "192.168.30.60:9092,192.168.30.61:9092,192.168.30.62:9092");
 
-		Producer producer = new Producer<Integer, String>(new ProducerConfig(properties));
-
-		producer.send(new KeyedMessage<Integer, String>("test", "message: 1"));
+		
+		String topic = "xxx";
+		Producer producer = new Producer<String, String>(new ProducerConfig(properties));
+		producer.send(new KeyedMessage<String, String>(topic, "message: 1"));
 	}
 }
