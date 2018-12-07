3db2579ec2528242594928ad75f53afd503491ee
yangrenzhi123 <yangrenzhi123@163.com>
2018-12-07 16:47:58 +0800

    1

--- com.yang.test.java/src/main/java/Test6.java
+++ com.yang.test.java/src/main/java/Test6.java
@@ -2,7 +2,7 @@ public class Test6 {
 
 	public static void main(String[] args) {
 
-		String newString = String.format("%07d", 5);
+		String newString = String.format("%011d", 5);
 		System.out.println("newString === " + newString);
 	}
 }
