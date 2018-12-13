d20db78646c23335c73ba70b99e5bd5a3eeba08b
yangrenzhi123 <yangrenzhi123@163.com>
2018-12-13 17:52:25 +0800

    1

--- com.yang.test.java.spring.cloud/zuul/src/main/java/com/yang/test/java/spring/cloud/zuul/TestZuul.java
+++ com.yang.test.java.spring.cloud/zuul/src/main/java/com/yang/test/java/spring/cloud/zuul/TestZuul.java
@@ -4,11 +4,13 @@ import javax.servlet.Filter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZuulProxy
 public class TestZuul {
 
