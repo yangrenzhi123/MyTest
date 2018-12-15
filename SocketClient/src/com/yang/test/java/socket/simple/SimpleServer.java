c3bd468e282a642aee0608a993db92e17bbacf0e
yangrenzhi123 <yangrenzhi123@163.com>
2018-12-15 09:38:29 +0800

    1

--- SocketClient/src/com/yang/test/java/socket/simple/SimpleServer.java
+++ SocketClient/src/com/yang/test/java/socket/simple/SimpleServer.java
@@ -5,17 +5,18 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-@SuppressWarnings("resource")
 public class SimpleServer {
 
 	public static void main(String[] args) throws IOException {
 		ServerSocket serverSocket = new ServerSocket(8099);
 		final Socket socket = serverSocket.accept();
-		while(true) {
-			InputStream is = socket.getInputStream();
-			byte[] b = new byte[1];
-			is.read(b);
-			System.out.println(new String(b));
-		}
+
+		InputStream is = socket.getInputStream();
+		byte[] b = new byte[1];
+		is.read(b);
+		System.out.println(new String(b));
+		
+		socket.close();
+		serverSocket.close();
 	}
 }
