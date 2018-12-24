7f06975026c1d568b721bda0b875c62697c80fbe
yangrenzhi123 <yangrenzhi123@163.com>
2018-12-24 18:59:47 +0800

    1

--- com.yang.test.java.jetcache/src/main/java/com/yang/test/java/jetcache/api/UserService.java
+++ com.yang.test.java.jetcache/src/main/java/com/yang/test/java/jetcache/api/UserService.java
@@ -1,10 +1,12 @@
 package com.yang.test.java.jetcache.api;
 
+import com.alicp.jetcache.anno.CachePenetrationProtect;
 import com.alicp.jetcache.anno.CacheType;
 import com.alicp.jetcache.anno.Cached;
 
 public interface UserService {
 
+	@CachePenetrationProtect //表示在多线程环境中同步加载数据
 	@Cached(name = "UserService.getUserById", cacheType = CacheType.REMOTE, expire = 3600)
 	String getUserById(String userId);
 }
