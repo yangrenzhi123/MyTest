aa4f56eefbfb793e805d8d9e4bf0924be9e1de5d
yangrenzhi123 <yangrenzhi123@163.com>
2019-04-02 08:29:38 +0800

    1

--- springboot.monitor/src/main/java/com/example/demo/MonitorForMysql.java
+++ springboot.monitor/src/main/java/com/example/demo/MonitorForMysql.java
@@ -15,34 +15,42 @@ public class MonitorForMysql {
 
 	@Autowired
 	Config config;
-	
+
 	static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
-	public  void execute() throws ClassNotFoundException, SQLException {
+	public void execute() throws ClassNotFoundException, SQLException {
 		List<String> l = config.getDbList();
-		
+
 		Class.forName(DRIVER);
 
-		for(String s : l) {
+		for (String s : l) {
+			MoniResult result = new MoniResult();
+			result.setName("Mysql");
+			result.setCheckTime(new Date());
+
 			String[] infos = s.split(" ");
-			
-			String DB_URL = "jdbc:mysql://"+infos[0]+"?useUnicode=true&characterEncoding=UTF-8&useSSL=false&allowMultiQueries=true";
-			
-			Connection conn = DriverManager.getConnection(DB_URL, infos[1], infos[2]);
+			String DB_URL = "jdbc:mysql://" + infos[0]
+					+ "?useUnicode=true&characterEncoding=UTF-8&useSSL=false&allowMultiQueries=true";
+
+			Connection conn = null;
+			try {
+				conn = DriverManager.getConnection(DB_URL, infos[1], infos[2]);
+			} catch (Exception e) {
+				result.setResult(0);
+				DemoApplication.result.put(infos[0] + " " + infos[1] + " ******", result);
+				continue;
+			}
+
 			conn.setAutoCommit(true);
 			PreparedStatement stmt = conn.prepareStatement("select user()");
 			boolean rs = stmt.execute();
 			stmt.close();
 			conn.close();
-			
-			MoniResult result = new MoniResult();
-			result.setName("Mysql");
-			result.setCheckTime(new Date());
-			
-			if(rs) {
+
+			if (rs) {
 				result.setResult(1);
 				DemoApplication.result.put(infos[0] + " " + infos[1] + " ******", result);
-			}else {
+			} else {
 				result.setResult(0);
 				DemoApplication.result.put(infos[0] + " " + infos[1] + " ******", result);
 			}
