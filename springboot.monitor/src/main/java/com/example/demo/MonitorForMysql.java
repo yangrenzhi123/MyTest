773909f40570f558d77bb814be8ddc4b9ac09829
yangrenzhi123 <yangrenzhi123@163.com>
2019-04-02 14:29:20 +0800

    1

--- springboot.monitor/src/main/java/com/example/demo/MonitorForMysql.java
+++ springboot.monitor/src/main/java/com/example/demo/MonitorForMysql.java
@@ -7,12 +7,16 @@ import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MonitorForMysql {
 
+	final static Logger logger = LoggerFactory.getLogger(MonitorForMysql.class);
+	
 	@Autowired
 	Config config;
 
@@ -29,13 +33,13 @@ public class MonitorForMysql {
 			result.setCheckTime(new Date());
 
 			String[] infos = s.split(" ");
-			String DB_URL = "jdbc:mysql://" + infos[0]
-					+ "?useUnicode=true&characterEncoding=UTF-8&useSSL=false&allowMultiQueries=true";
+			String DB_URL = "jdbc:mysql://" + infos[0] + "?useUnicode=true&characterEncoding=UTF-8&useSSL=false&allowMultiQueries=true&allowPublicKeyRetrieval=true";
 
 			Connection conn = null;
 			try {
 				conn = DriverManager.getConnection(DB_URL, infos[1], infos[2]);
 			} catch (Exception e) {
+				logger.error("", e);
 				result.setResult(0);
 				DemoApplication.result.put(infos[0] + " " + infos[1] + " ******", result);
 				continue;
