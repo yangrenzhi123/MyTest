779914174a170bdbe4cf03e0fe79630054811d87
yangrenzhi123 <yangrenzhi123@163.com>
2019-04-12 13:31:46 +0800

    1

--- com.yang.test.java.jedis/src/main/java/com/yang/test/java/jedis/RedisSimple.java
+++ com.yang.test.java.jedis/src/main/java/com/yang/test/java/jedis/RedisSimple.java
@@ -1,24 +1,21 @@
 package com.yang.test.java.jedis;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
-import redis.clients.jedis.HostAndPort;
-import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.Jedis;
 
 public class RedisSimple {
 	public static void main(String[] args) throws IOException {
-//		Jedis j = new Jedis("192.168.30.120", 6379);
+		Jedis j = new Jedis("192.168.10.226", 6379);
 
-		Set<HostAndPort> nodes = new HashSet<HostAndPort>();
-		nodes.add(new HostAndPort("192.168.10.20", 7001));
-		nodes.add(new HostAndPort("192.168.10.20", 7002));
-		nodes.add(new HostAndPort("192.168.10.20", 7003));
-		nodes.add(new HostAndPort("192.168.10.22", 7004));
-		nodes.add(new HostAndPort("192.168.10.22", 7005));
-		nodes.add(new HostAndPort("192.168.10.22", 7006));
-		JedisCluster j = new JedisCluster(nodes);
+//		Set<HostAndPort> nodes = new HashSet<HostAndPort>();
+//		nodes.add(new HostAndPort("192.168.10.20", 7001));
+//		nodes.add(new HostAndPort("192.168.10.20", 7002));
+//		nodes.add(new HostAndPort("192.168.10.20", 7003));
+//		nodes.add(new HostAndPort("192.168.10.22", 7004));
+//		nodes.add(new HostAndPort("192.168.10.22", 7005));
+//		nodes.add(new HostAndPort("192.168.10.22", 7006));
+//		JedisCluster j = new JedisCluster(nodes);
 
 //		Set<HostAndPort> nodes = new HashSet<HostAndPort>();
 //		nodes.add(new HostAndPort("192.168.30.62", 7001));
@@ -47,16 +44,16 @@ public class RedisSimple {
 //		nodes.add(new HostAndPort("192.168.10.240", 7006));
 //		JedisCluster j = new JedisCluster(nodes);
 
-		String key = "h_equipment00012180904405";
-//		j.select(3);
-//		System.out.println(j.get(key));
+		String key = "55555555555";
+		j.select(3);
+		System.out.println(j.get(key));
 //		j.del(key);
 
 //		Map<String, String> m = new HashMap<String, String>();
 //		m.put("1", "1");
 //		j.hmset(key, m);
 
-		System.out.println(j.hgetAll(key));
+//		System.out.println(j.hgetAll(key));
 
 		j.close();
 	}
