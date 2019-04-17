b504ac164db37b3b0c147b06ac6dbbc0c5b925a6
yangrenzhi123 <yangrenzhi123@163.com>
2019-04-17 19:49:18 +0800

    1

--- com.yang.test.java.pure/src/main/java/TestSendToNetty.java
+++ com.yang.test.java.pure/src/main/java/TestSendToNetty.java
@@ -1,8 +1,3 @@
-
-
-/**
- * 箱体发送投放记录
- */
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,21 +7,16 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 public class TestSendToNetty {
-
-	public final static String URL = "192.168.10.239";
-	public final static int PORT = 3113;
 
 	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
 //		int c1 = Integer.parseInt(args[0]);
 //		int c2 = Integer.parseInt(args[1]);
 		
-		int c1 = 16308; //设备数 16254:401--702;10044:401--587
-		int c2 = Integer.parseInt(args[0]); //发送次数
-		String type = "2";//分类箱==2;回收箱==4
+		int c1 = 1; //设备数
+		int c2 = 10000; //发送次数
 
 		
 
@@ -47,7 +37,7 @@ public class TestSendToNetty {
 		List<InputStream> isl = new ArrayList<InputStream>();
 		List<Socket> sl = new ArrayList<Socket>();
 		for (int i = 0; i < c1; i++) {
-			Socket request = new Socket(URL, PORT);
+			Socket request = new Socket("192.168.30.121", 3113);
 			OutputStream os = request.getOutputStream();
 			InputStream is = request.getInputStream();
 			isl.add(is);
@@ -57,102 +47,70 @@ public class TestSendToNetty {
 		System.out.println("连接耗时："+(System.currentTimeMillis() - a));
 
 		
-		/*分类：【分类箱00000[0001-2000][01-09]2[01-06]】
-		
-		00000  0001-2000  01-03 201
-		 */
+
 		final CountDownLatch latch = new CountDownLatch(c1);
 		
-		List<String> deviceNos = new ArrayList<>();//设备类型
-		List<String> rqcodes = new ArrayList<>();//二维码
-		Random random = new Random();
-		for (int i = 401; i <= 702; i++) {
-			//String deviceNo = String.format("%014d", 61000000000001L+i);
-			//分类箱：【二分类箱00000[0001-2000][01-09]2[01-06]】 
-			//回收箱：【可回收箱00000[0001-2000][01-09]4[01-02]】
-			//业主二维码：LYZH6[0001-2000][01-09][001-400]01
-			for(int j =1;j<=9;j++) {
-				
-				for(int x =1;x<=6;x++) {
-					/**
-					 * 反方及：00000022101802
-						回收箱：00000022101402
-						分类箱：00000022101202
-						二维码：LYZH602210107801
-					 */
-					//String deviceNo = "00000022101202";
-					//String rqcode = "LYZH602210107801";
-					
-					String deviceNo = "00000";
-					String tenant = String.format("%04d", i);
-					String communityNum = String.format("%02d", j);
-					String equipEnd = String.format("%02d", x);
-					deviceNo = deviceNo+tenant+communityNum+type+equipEnd;
-					String userNum = 100+x+"";
-					String rqcode = "LYZH6"+tenant+communityNum+userNum+"01";
-					
-					String aa = deviceNo.substring(0, 2);
-					String bb = deviceNo.substring(2, 4);
-					String cc = deviceNo.substring(4, 6);
-					String dd = deviceNo.substring(6, 8);
-					String ee = deviceNo.substring(8, 10);
-					String ff = deviceNo.substring(10, 12);
-					String gg = deviceNo.substring(12, 14);
-					if(aa.equals("55")) {
-						aa = "5401";
-					}
-					if(aa.equals("54")) {
-						aa = "5402";
-					}
-					
-					if(bb.equals("55")) {
-						bb = "5401";
-					}
-					if(bb.equals("54")) {
-						bb = "5402";
-					}
-					
-					if(cc.equals("55")) {
-						cc = "5401";
-					}
-					if(cc.equals("54")) {
-						cc = "5402";
-					}
-					
-					if(dd.equals("55")) {
-						dd = "5401";
-					}
-					if(dd.equals("54")) {
-						dd = "5402";
-					}
-					
-					if(ee.equals("55")) {
-						ee = "5401";
-					}
-					if(ee.equals("54")) {
-						ee = "5402";
-					}
-					
-					if(ff.equals("55")) {
-						ff = "5401";
-					}
-					if(ff.equals("54")) {
-						ff = "5402";
-					}
-					
-					if(gg.equals("55")) {
-						gg = "5401";
-					}
-					if(gg.equals("54")) {
-						gg = "5402";
-					}
-					
-					deviceNo = aa + bb + cc + dd + ee + ff + gg;
-					deviceNos.add(deviceNo);
-					rqcodes.add(rqcode);
-				}
-				
+		List<String> deviceNos = new ArrayList<>();
+		for (int i = 0; i < c1; i++) {
+			String deviceNo = String.format("%014d", 61000000000001L+i);
+			String aa = deviceNo.substring(0, 2);
+			String bb = deviceNo.substring(2, 4);
+			String cc = deviceNo.substring(4, 6);
+			String dd = deviceNo.substring(6, 8);
+			String ee = deviceNo.substring(8, 10);
+			String ff = deviceNo.substring(10, 12);
+			String gg = deviceNo.substring(12, 14);
+			if(aa.equals("55")) {
+				aa = "5401";
 			}
+			if(aa.equals("54")) {
+				aa = "5402";
+			}
+
+			if(bb.equals("55")) {
+				bb = "5401";
+			}
+			if(bb.equals("54")) {
+				bb = "5402";
+			}
+
+			if(cc.equals("55")) {
+				cc = "5401";
+			}
+			if(cc.equals("54")) {
+				cc = "5402";
+			}
+
+			if(dd.equals("55")) {
+				dd = "5401";
+			}
+			if(dd.equals("54")) {
+				dd = "5402";
+			}
+
+			if(ee.equals("55")) {
+				ee = "5401";
+			}
+			if(ee.equals("54")) {
+				ee = "5402";
+			}
+
+			if(ff.equals("55")) {
+				ff = "5401";
+			}
+			if(ff.equals("54")) {
+				ff = "5402";
+			}
+
+			if(gg.equals("55")) {
+				gg = "5401";
+			}
+			if(gg.equals("54")) {
+				gg = "5402";
+			}
+			
+			deviceNo = aa + bb + cc + dd + ee + ff + gg;
+			deviceNos.add(deviceNo);
 		}
 		
 		a = System.currentTimeMillis();
@@ -163,25 +121,10 @@ public class TestSendToNetty {
 				public void run() {
 					OutputStream os = osl.get(j);
 					InputStream is = isl.get(j);
-					//LYZH618840405901
+					
 					for(int m=0;m<c2;m++) {
-						String bin = "554000E00102";
-						String equipNo = deviceNos.get(j);//设备号
-						String rqcode = rqcodes.get(j);
-						//System.out.println("设备号："+equipNo+"; 业主二维码："+rqcode);
-						String type = "04";//01：玻璃/02：塑料/03：纸张/04：金属/05：厨余/06：其他/0A：有毒有害/0B：可回收/0C：可腐烂/0D：不可腐烂
-						String weight = "E80301";//E803(10.00kg)      9C04(25.00kg)
-						//weight = "409C01";//减重 409C（-255.36kg）
-						//weight = "A00F01";//超重 
-						String other = "000000000000";//其他预留位
-						String area = timeTo16()+rqcodeTo16(rqcode)+type+weight+other;
-						String CRC_TOP = "A4";
-						String CRC_Low = "B6";
-						String end = "55";
-						String protocol_55= bin+equipNo+area+CRC_TOP+CRC_Low+end;
-						byte[] bs = hexString2Bytes(protocol_55);
 																								   //时间1301120F0B04                  //LYZH181225000101                         //垃圾类型
-						//byte[] bs = hexString2Bytes("554000E00102"+/*deviceNos.get(j)*/"00000011223366"+timeTo16()+"4c595a4831383132323530303031303100000000000000000000000000000000"+"04"+"E80300000000000000A4B655");
+						byte[] bs = hexString2Bytes("554000E00102"+/*deviceNos.get(j)*/"00000011223366"+timeTo16()+"4c595a4831383132323530303031303100000000000000000000000000000000"+"04"+"E80300000000000000A4B655");
 						try {
 							os.write(bs);
 							is.read(new byte[1024]);
@@ -189,11 +132,11 @@ public class TestSendToNetty {
 							e.printStackTrace();
 						}
 						
-						/*try {
+						try {
 							Thread.sleep(1000);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
-						}*/
+						}
 					}
 					latch.countDown();
 				}
@@ -230,25 +173,6 @@ public class TestSendToNetty {
 			}
 			return b;
 		}
-	}
-	
-	/*
-	 * 获取_rqcode 16进制   32位 不足补0
-	 */
-	public static String rqcodeTo16(String s) {
-	    String str = "";
-	    for (int i = 0; i < s.length(); i++) {
-	        int ch = (int) s.charAt(i);
-	        String s4 = Integer.toHexString(ch);
-	        str = str + s4;
-	    }
-	    if(str.length()<64) {
-	    	int num = 64-str.length();
-	    	for(int i = 0;i<num;i++) {
-	    		str = str+"0";
-	    	}
-	    }
-	    return str;
 	}
 	
 	private static byte charToByte(char c) {
