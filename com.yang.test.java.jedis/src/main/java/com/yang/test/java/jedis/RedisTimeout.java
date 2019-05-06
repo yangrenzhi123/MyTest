f82839532dbcff085e08f28971cb7bdab7abd92e
yangrenzhi123 <yangrenzhi123@163.com>
2019-05-06 18:29:29 +0800

    1

--- com.yang.test.java.jedis/src/main/java/com/yang/test/java/jedis/RedisTimeout.java
+++ com.yang.test.java.jedis/src/main/java/com/yang/test/java/jedis/RedisTimeout.java
@@ -6,24 +6,13 @@ import redis.clients.jedis.Jedis;
 
 public class RedisTimeout {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		Jedis j = new Jedis("172.28.51.33", 6379);
+		Jedis j = new Jedis("192.168.10.230", 7001);
 
 		String key = "testKey";
 		j.setex(key, 5, "YangRenZhi");
+		
+		System.out.println(j.get(key));
 
-		while (true) {
-			String a = j.get(key);
-
-			if (a == null) {
-				break;
-			}
-
-			System.out.println(a);
-
-			Thread.sleep(1000);
-			
-			j.set(key, System.currentTimeMillis()+"");
-		}
 		j.close();
 	}
 }
--- com.yang.test.java/src/main/java/Testtt.java
+++ com.yang.test.java/src/main/java/Testtt.java
@@ -1,38 +1,16 @@
+
+import java.time.*;
+import java.time.temporal.TemporalAdjusters;
+
 public class Testtt {
 
+	
+	
 	public static void main(String[] args) {
-		String s = "556454995554";
-		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<s.length()/2;i++) {
-			String s1 = s.substring(i*2, (i+1)*2);
-			if(s1.equals("55")) {
-				s1 = "5401";
-			}
-			if(s1.equals("54")) {
-				s1 = "5402";
-			}
-			sb.append(s1);
-		}
-		System.out.println(sb.toString());
-		
-		
-		//System.out.println(turnHex(intToHex(0)));
-	}
-
-	public static String intToHex(int i) {
-		String s = Integer.toHexString(i).toUpperCase();
-		if (s.length() == 1) {
-			s = "000" + s;
-		}
-		if (s.length() == 3) {
-			s = "0" + s;
-		}
-		return s;
-	}
-
-	public static String turnHex(String hex) {
-		String s2 = hex.substring(0, 2);
-		String s1 = hex.substring(2, 4);
-		return s1 + s2;
+		LocalDateTime of = LocalDateTime.of(LocalDate.now().with(TemporalAdjusters.firstDayOfNextMonth()), LocalTime.of(0, 0, 0));
+		Duration between = Duration.between(LocalDateTime.now(), of);
+		System.out.println(between.getSeconds());
+		int second = (int) between.getSeconds();
+		System.out.println(second);
 	}
 }
