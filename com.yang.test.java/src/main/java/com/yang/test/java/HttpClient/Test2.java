b7c31b40455e1fc05e75091c9739c0be96f55742
yangrenzhi123 <yangrenzhi123@163.com>
2019-07-05 10:36:09 +0800

    1

--- com.yang.test.java/src/main/java/com/yang/test/java/HttpClient/Test2.java
+++ com.yang.test.java/src/main/java/com/yang/test/java/HttpClient/Test2.java
@@ -25,17 +25,17 @@ public class Test2 {
 		String s = ss[1];
 		String[] sss = s.split(",");
 		int length = sss.length;
-		System.out.println(sss[length - 3] + " " + sss[length - 2]);
-		System.out.println(sss[length - 4] + " " + sss[length - 5]);
-		System.out.println(sss[length - 6] + " " + sss[length - 7]);
-		System.out.println(sss[length - 8] + " " + sss[length - 9]);
-		System.out.println(sss[length - 10] + " " + sss[length - 11]);
-		System.out.println(sss[length - 12] + " " + sss[length - 13]);
+		System.out.println(sss[length -  3] + " " + sss[length - 2]);
+		System.out.println(sss[length -  4] + " " + Integer.parseInt(sss[length -  5])/100);
+		System.out.println(sss[length -  6] + " " + Integer.parseInt(sss[length -  7])/100);
+		System.out.println(sss[length -  8] + " " + Integer.parseInt(sss[length -  9])/100);
+		System.out.println(sss[length - 10] + " " + Integer.parseInt(sss[length - 11])/100);
+		System.out.println(sss[length - 12] + " " + Integer.parseInt(sss[length - 13])/100);
 		System.out.println("");
-		System.out.println(sss[length - 22] + " " + sss[length - 23]);
-		System.out.println(sss[length - 20] + " " + sss[length - 21]);
-		System.out.println(sss[length - 18] + " " + sss[length - 19]);
-		System.out.println(sss[length - 16] + " " + sss[length - 17]);
-		System.out.println(sss[length - 14] + " " + sss[length - 15]);
+		System.out.println(sss[length - 22] + " " + Integer.parseInt(sss[length - 23])/100);
+		System.out.println(sss[length - 20] + " " + Integer.parseInt(sss[length - 21])/100);
+		System.out.println(sss[length - 18] + " " + Integer.parseInt(sss[length - 19])/100);
+		System.out.println(sss[length - 16] + " " + Integer.parseInt(sss[length - 17])/100);
+		System.out.println(sss[length - 14] + " " + Integer.parseInt(sss[length - 15])/100);
 	}
 }
