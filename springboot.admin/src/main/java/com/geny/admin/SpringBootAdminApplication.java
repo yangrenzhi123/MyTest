20eba0db16cd902cafb7727e3a8e31392bd0bff9
yangrenzhi123 <yangrenzhi123@163.com>
2019-07-12 14:45:50 +0800

    1

--- springboot.admin/src/main/java/com/geny/admin/SpringBootAdminApplication.java
+++ springboot.admin/src/main/java/com/geny/admin/SpringBootAdminApplication.java
@@ -2,15 +2,11 @@ package com.geny.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
-@Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
-@EnableEurekaClient
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
