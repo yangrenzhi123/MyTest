2bfa523b98c7ebfb75c9d76ebf6115a4f5632e56
yangrenzhi123 <yangrenzhi123@163.com>
2019-08-08 10:19:02 +0800

    1

--- springboot.Scheduled/src/main/java/com/yang/test/java/springboot/Scheduled/MyTask.java
+++ springboot.Scheduled/src/main/java/com/yang/test/java/springboot/Scheduled/MyTask.java
@@ -6,18 +6,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyTask {
 
-	
-	
-	
-	//	秒（0~59）
-	//	分钟（0~59）
-	//	小时（0~23）
-	//	天（月）（0~31，但是你需要考虑你月的天数）
-	//	月（0~11）
-	//	天（星期）（1~7 1=SUN 或 SUN，MON，TUE，WED，THU，FRI，SAT）
-	//	年份（1970－2099）
-    @Scheduled(cron = "0/10 * * * * * *")
-    public void work() {
-        System.out.println(1);
-    }
+	// 秒（0~59）
+	// 分钟（0~59）
+	// 小时（0~23）
+	// 天（月）（0~31，但是你需要考虑你月的天数）
+	// 月（0~11）
+	// 天（星期）（1~7 1=SUN 或 SUN，MON，TUE，WED，THU，FRI，SAT）
+	// 年份（1970－2099）
+	@Scheduled(cron = "0/10 * * * * *")
+	public void work() throws InterruptedException {
+		System.out.println(System.currentTimeMillis());
+		
+		
+		Thread.sleep(100000L);
+	}
 }
