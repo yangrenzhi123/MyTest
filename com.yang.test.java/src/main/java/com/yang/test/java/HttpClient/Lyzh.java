8cfb8f3260411ec728842b44022c95dad7f37ff3
yangrenzhi123 <yangrenzhi123@163.com>
2019-09-30 09:38:09 +0800

    1

--- com.yang.test.java/src/main/java/com/yang/test/java/HttpClient/Lyzh.java
+++ com.yang.test.java/src/main/java/com/yang/test/java/HttpClient/Lyzh.java
@@ -11,7 +11,7 @@ import org.apache.http.impl.client.HttpClients;
 public class Lyzh {
 
 	public static void main(String[] args) throws ClientProtocolException, IOException {
-		String cookie = "lyzh-saas=s%3AJExoaP4TjrTqfmFczc8xevKXE0Nxv6KF.IVvTA1Gzn2tQoqfR2scsBjySjqp1mAKuAaiXimOudSE";
+		String cookie = "lyzh-saas=s%3ABoUzPPL1F-TATKi0Ds27cZPewX25-Pz-.nkCilGcoO0JRcf4LJBuSStJZlkGQZIU6eyTdN%2FsPlX4";
 		
 		HttpPost post = null;
 		StringEntity entity = null;
@@ -96,6 +96,29 @@ public class Lyzh {
 		c("http://hfyhxm.lyzhyun.com:8000/api-fl/equipfixedvideo/l.do", cookie, 
 		"{\"pageNum\":1,\"qycs\":\"5\",\"regionid\":\"babb5342-6ade-4158-b070-baf1d18dae1c\",\"qylx\":\"1\",\"communityid\":null,\"bdzt\":null,\"orderid\":null,\"sbmc\":null,\"productid\":\"58fd4d48-cbe5-430c-8cca-8b5b39469bd5\",\"pageSize\":10,\"orderSjly\":0}",
 		"基础信息-设备管理-固定视频，查询层级(中国)");
+		
+		
+		//-----------------------------生活垃圾----------------------------------------------
+		
+		c("http://hfyhxm.lyzhyun.com:8000/api-fl/lifegarbagethrow/l.do", cookie, 
+		"{\"pageNum\":1,\"regionid\":\"babb5342-6ade-4158-b070-baf1d18dae1c\",\"qylx\":\"1\",\"communityid\":null,\"jlly\":null,\"ljlx\":null,\"yhlx\":null,\"dateStart\":\"2019-09-01 00:00:00\",\"dateEnd\":\"2019-09-30 23:59:59\",\"zhnc\":null,\"sjhm\":null,\"qrcode\":null,\"queryType\":1,\"productid\":\"58fd4d48-cbe5-430c-8cca-8b5b39469bd5\",\"pageSize\":10,\"orderSjly\":0}",
+		"生活垃圾-前端分类-投放查询，查询层级(中国)");
+		
+		c("http://hfyhxm.lyzhyun.com:8000/api-fl/lifegarbage/l.do", cookie, 
+		"{\"pageNum\":1,\"regionid\":\"babb5342-6ade-4158-b070-baf1d18dae1c\",\"qylx\":\"1\",\"communityid\":null,\"ljlx\":null,\"sblx\":null,\"stringkssj\":\"2019-09-01 00:00:00\",\"stringjssj\":\"2019-09-30 23:59:59\",\"productid\":\"58fd4d48-cbe5-430c-8cca-8b5b39469bd5\",\"pageSize\":10,\"orderSjly\":0}",
+		"生活垃圾-前端分类-收集员收集查询，查询层级(中国)");
+		
+		c("http://hfyhxm.lyzhyun.com:8000/api-fl/inspectrecord/l.do", cookie, 
+		"{\"pageNum\":1,\"regionid\":\"babb5342-6ade-4158-b070-baf1d18dae1c\",\"qylx\":\"1\",\"communityid\":null,\"kssj\":\"2019-09-01 00:00:00\",\"jssj\":\"2019-09-30 23:59:59\",\"yhlx\":null,\"zsxm\":null,\"yhxx\":null,\"yhsj\":null,\"yhkh\":null,\"productid\":\"58fd4d48-cbe5-430c-8cca-8b5b39469bd5\",\"pageSize\":10,\"orderSjly\":0}",
+		"生活垃圾-前端分类-巡检查询，查询层级(中国)");
+		
+		c("http://hfyhxm.lyzhyun.com:8000/api-fl/garbagebagpull/l.do", cookie, 
+		"{\"pageNum\":1,\"regionid\":\"babb5342-6ade-4158-b070-baf1d18dae1c\",\"qylx\":\"1\",\"communityid\":null,\"kssj\":\"2019-09-01 00:00:00\",\"jssj\":\"2019-09-30 23:59:59\",\"fdjlx\":null,\"yhlx\":null,\"zsxm\":null,\"sjhm\":null,\"qrcode\":null,\"lyfs\":null,\"productid\":\"58fd4d48-cbe5-430c-8cca-8b5b39469bd5\",\"pageSize\":10,\"orderSjly\":0}",
+		"生活垃圾-前端分类-领用查询，查询层级(中国)");
+		
+		c("http://hfyhxm.lyzhyun.com:8000/api-fl/inspectrecord/l/byMonth.do", cookie, 
+		"{\"pageNum\":1,\"regionid\":\"babb5342-6ade-4158-b070-baf1d18dae1c\",\"qylx\":\"1\",\"communityid\":null,\"xjyf\":null,\"xjy\":null,\"productid\":\"58fd4d48-cbe5-430c-8cca-8b5b39469bd5\",\"pageSize\":10,\"orderSjly\":0}",
+		"生活垃圾-前端分类-月巡检查询，查询层级(中国)");
 	}
 
 	public static void c(String url, String cookie, String json, String title) throws ClientProtocolException, IOException {
