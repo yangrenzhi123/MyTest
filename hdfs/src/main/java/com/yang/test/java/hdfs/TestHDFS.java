4b91bce4ac4fd315160b9c6d88da26c277094687
yangrenzhi123 <yangrenzhi123@163.com>
2019-11-15 19:35:34 +0800

    1

--- hdfs/src/main/java/com/yang/test/java/hdfs/TestHDFS.java
+++ hdfs/src/main/java/com/yang/test/java/hdfs/TestHDFS.java
@@ -1,6 +1,8 @@
 package com.yang.test.java.hdfs;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -8,16 +10,15 @@ import org.apache.hadoop.fs.Path;
 
 public class TestHDFS {
 
-	public static void main(String[] args) {
+	
+	/*/opt/modules/app/hadoop-2.6.5/data/tmp/dfs/data/current/BP-2097822308-192.168.18.231-1573805007663/current/finalized/subdir0/subdir0*/
+	public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(new URI("hdfs://192.168.18.231:8020"), conf, "root");
 
-		try {
-			Configuration conf = new Configuration();
-			FileSystem fs = FileSystem.get(new URI("hdfs://192.168.30.151:8020"), conf, "root");
+		fs.copyFromLocalFile(false, true, new Path("C:/test.xml"), new Path("/demo1/test.xml"));
 
-			fs.copyToLocalFile(false, new Path("/demo1/core-site.xml"), new Path("C:/"), true);
-			fs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		fs.copyToLocalFile(false, new Path("/demo1/test.xml"), new Path("C:/test2.xml"), true);
+		fs.close();
 	}
 }
