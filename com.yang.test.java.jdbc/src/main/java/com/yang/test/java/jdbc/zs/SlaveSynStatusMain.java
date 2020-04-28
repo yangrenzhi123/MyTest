cb4249aa3a3b2efe7b16483f572a2dbe3f02e8a8
yangrenzhi123 <yangrenzhi123@163.com>
2020-04-28 14:28:21 +0800

    1

--- com.yang.test.java.jdbc/src/main/java/com/yang/test/java/jdbc/zs/SlaveSynStatusMain.java
+++ com.yang.test.java.jdbc/src/main/java/com/yang/test/java/jdbc/zs/SlaveSynStatusMain.java
@@ -5,10 +5,14 @@ import java.sql.SQLException;
 public class SlaveSynStatusMain {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		JDBCSimple22.doIt();
-		JDBCSimple224.doIt();
-		JDBCSimple26.doIt();
-		JDBCSimple2243307.doIt();
-		JDBCSimple263307.doIt();
+		// 20 主库
+		JDBCSimple22.doIt(); // 从库
+		JDBCSimple26.doIt(); // 从库
+		JDBCSimple224.doIt(); // 从库
+		JDBCSimple2243307.doIt(); // 备主
+		JDBCSimple263307.doIt(); // 备从
+
+		// dw主192.168.10.224:3308
+		JDBCSimple263308.doIt(); // dw从192.168.10.26:3308
 	}
 }
