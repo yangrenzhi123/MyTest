19617e9eefee8f7ba2695818f2c88e9f54c78cd3
yangrenzhi123 <yangrenzhi123@163.com>
2020-05-10 10:35:21 +0800

    1

--- SocketClient/src/com/yang/test/java/socket/simple/SimpleClient2.java
+++ SocketClient/src/com/yang/test/java/socket/simple/SimpleClient2.java
@@ -9,25 +9,22 @@ import java.net.UnknownHostException;
 public class SimpleClient2 {
 
 	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
-
-		Socket request = new Socket("127.0.0.1", 3113);
+		Socket request = new Socket("127.0.0.1", 8099);
 
 		OutputStream os = request.getOutputStream();
 
-		os.write("1".getBytes());
-		byte[] b = new byte[1024];
-		InputStream is = request.getInputStream();
-		is.read(b);
-		System.out.println(new String(b));
-		
-		os.write("1".getBytes());
-		b = new byte[1024];
-		is = request.getInputStream();
-		is.read(b);
-		System.out.println(new String(b));
-		
-		Thread.sleep(100000L);
-		
+		os.write("aaa".getBytes());
+//		byte[] b = new byte[1024];
+//		InputStream is = request.getInputStream();
+//		is.read(b);
+//		System.out.println(new String(b));
+//		
+//		os.write("1".getBytes());
+//		b = new byte[1024];
+//		is = request.getInputStream();
+//		is.read(b);
+//		System.out.println(new String(b));
+
 		os.close();
 		request.close();
 	}
--- SocketClient/src/com/yang/test/java/socket/simple/SimpleServer.java
+++ SocketClient/src/com/yang/test/java/socket/simple/SimpleServer.java
@@ -9,14 +9,61 @@ public class SimpleServer {
 
 	public static void main(String[] args) throws IOException {
 		ServerSocket serverSocket = new ServerSocket(8099);
-		final Socket socket = serverSocket.accept();
+		int i = 0;
+		while (true) {
+			final Socket socket = serverSocket.accept();
+			new Thread(new Runnable() {
+				public void run() {
+					try {
+						A:while(true) {
+							InputStream is = socket.getInputStream();
+							while(true) {
+								byte[] b = new byte[2];
+								int a = is.read(b);
+								int byteArrayAvalibe = 2;
+								if(a == -1) {
+									byteArrayAvalibe = byteArrayAvalibe(b);
+								}
+								if (a == -1 && byteArrayAvalibe == 0) {
+									System.out.println("Á¬½ÓÒì³£");
+									break A;
+								}
+								
+								byte[] c = new byte[byteArrayAvalibe];
+								System.arraycopy(b, 0, c, 0, byteArrayAvalibe);
+								
+								System.out.println("--" + new String(c));
+								if(a == -1) {
+									break;
+								}
+							}
+						}
+					}catch(Exception e) {
+						try {
+							socket.close();
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+						e.printStackTrace();
+					}
+				}
+			}).start();
 
-		InputStream is = socket.getInputStream();
-		byte[] b = new byte[1];
-		is.read(b);
-		System.out.println(new String(b));
-		
-		socket.close();
+			if (i++ > 10000) {
+				break;
+			}
+		}
 		serverSocket.close();
+	}
+	
+	public static int byteArrayAvalibe(byte[] b) {
+		int j = 0;
+		for (int i = 0; i < b.length; i++) {
+			if (b[i] == 0) {
+				j = i;
+				break;
+			}
+		}
+		return j;
 	}
 }
