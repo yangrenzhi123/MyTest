8a456d042ec70e9ba46895c0784ffe4e7ec3debd
yangrenzhi123 <yangrenzhi123@163.com>
2020-05-10 11:28:49 +0800

    1

--- SocketClient/src/com/yang/test/java/socket/simple/TCPRouter.java
+++ SocketClient/src/com/yang/test/java/socket/simple/TCPRouter.java
@@ -8,32 +8,99 @@ import java.net.Socket;
 
 public class TCPRouter {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(8099);
-		final Socket socket = serverSocket.accept();
+		final Socket receivedSocket = serverSocket.accept();
+		
 		final Socket mysqlSocket = new Socket("192.168.10.229", 5306);
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					A:while(true) {
+						InputStream mysqlIS = mysqlSocket.getInputStream();
+						while(true) {
+							int length = 10;
+							byte[] b = new byte[length];
+							
+							int a = mysqlIS.read(b);
+							
+							int byteArrayAvalibe = a;
+							if(a == -1) {
+								byteArrayAvalibe = byteArrayAvalibe(b);
+							}
+							if (a == -1 && byteArrayAvalibe == 0) {
+								System.out.println("连接异常");
+								break A;
+							}
+							
+							byte[] c = new byte[byteArrayAvalibe];
+							System.arraycopy(b, 0, c, 0, byteArrayAvalibe);
+							OutputStream receivedOS = receivedSocket.getOutputStream();
+							receivedOS.write(c);
+							
+							if(a == -1) {
+								break;
+							}
+						}
+					}
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
 		
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					A:while(true) {
+						InputStream is = receivedSocket.getInputStream();
+						while(true) {
+							int length = 10;
+							byte[] b = new byte[length];
 
+							int a = is.read(b);
+							
+							int byteArrayAvalibe = a;
+							if(a == -1) {
+								byteArrayAvalibe = byteArrayAvalibe(b);
+							}
+							if (a == -1 && byteArrayAvalibe == 0) {
+								System.out.println("连接异常");
+								break A;
+							}
+							
+							byte[] c = new byte[byteArrayAvalibe];
+							System.arraycopy(b, 0, c, 0, byteArrayAvalibe);
+							OutputStream os = mysqlSocket.getOutputStream();
+							os.write(c);
+							
+							if(a == -1) {
+								break;
+							}
+						}
+					}
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+
+		Thread.sleep(1000000);
 		
-		InputStream is = socket.getInputStream();
-		byte[] b = new byte[1024];
-		is.read(b, 0, b.length);
-		System.out.println("-----1----" + new String(b));
-
-
-		OutputStream os = mysqlSocket.getOutputStream();
-		os.write(b);
-		InputStream mysqlIS = mysqlSocket.getInputStream();
-
-		byte[] c = new byte[1024];
-		mysqlIS.read(c);
-
-		OutputStream receivedOS = socket.getOutputStream();
-		receivedOS.write(c);
-
-		socket.close();
+		receivedSocket.close();
 		serverSocket.close();
 		mysqlSocket.close();
+	}
+	
+	public static int byteArrayAvalibe(byte[] b) {
+		int j = 0;
+		for (int i = b.length - 1; i >= 0; i--) {
+			if (b[i] != 0) {
+				j = i;
+				break;
+			}
+		}
+		return j;
 	}
 }
 
