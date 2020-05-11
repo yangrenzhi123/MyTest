417d52d39503df521a13dd2978b553c3558abbc3
yangrenzhi123 <yangrenzhi123@163.com>
2020-05-11 08:56:53 +0800

    1

---
+++ SocketClient/src/com/yang/test/java/socket/simple2/Client.java
@@ -0,0 +1,21 @@
+package com.yang.test.java.socket.simple2;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+		Socket request = new Socket("127.0.0.1", 8099);
+
+		OutputStream os = request.getOutputStream();
+
+		os.write("aaa".getBytes());
+		os.write("aaa".getBytes());
+
+		os.close();
+		request.close();
+	}
+}
---
+++ SocketClient/src/com/yang/test/java/socket/simple2/Server.java
@@ -0,0 +1,29 @@
+package com.yang.test.java.socket.simple2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) throws IOException {
+		final ServerSocket serverSocket = new ServerSocket(8099);
+		final Socket socket = serverSocket.accept();
+		final InputStream is = socket.getInputStream();
+		while (true) {
+			byte[] b = new byte[2];
+			System.out.print("开始读取数据");
+			int a = is.read(b);
+			System.out.print("，a：" + a);
+			System.out.println("，内容：" + new String(b));
+
+			if (a == -1) {
+				System.out.println("连接异常");
+				break;
+			}
+		}
+
+		serverSocket.close();
+	}
+}
