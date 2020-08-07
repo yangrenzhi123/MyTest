cf658a6f4f3096be120f20e973cdb591ca816dfd
yangrenzhi123 <yangrenzhi123@163.com>
2020-08-07 13:44:06 +0800

    1

--- springboot/src/main/java/com/yang/test/java/springboot/Springboot.java
+++ springboot/src/main/java/com/yang/test/java/springboot/Springboot.java
@@ -1,6 +1,8 @@
 package com.yang.test.java.springboot;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,21 +13,14 @@ import com.yang.test.java.springboot.entity.Man;
 @SpringBootApplication
 public class Springboot {
 
-	@Bean(name = "t1")
-	public Man test() {
-		return new Man();
-	}
-
-	@Bean(name = "t1")
-	public Man test2() {
-		return new Man();
-	}
-
-	public Man test3() {
-		return new Man();
-	}
-
-	public Man test4() {
+	@Bean
+	public Man test() throws ClassNotFoundException, IOException {
+		URL[] urls = new URL[] {new URL("file:/c:/classes/")};
+		URLClassLoader cl = new URLClassLoader(urls);
+		Class<?> c = cl.loadClass("com.yang.test.java.springboot.controller.TestController");
+		System.out.println(c.getPackage());
+		cl.close();
+		
 		return new Man();
 	}
 
