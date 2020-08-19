07043f120c846e0fb1a8ae6d38ced8fc18c540c3
yangrenzhi123 <yangrenzhi123@163.com>
2020-08-19 15:51:37 +0800

    1

--- com.yang.test.java.jdbc/src/main/java/com/yang/test/java/jdbc/zs/SlaveSynStatusMain.java
+++ com.yang.test.java.jdbc/src/main/java/com/yang/test/java/jdbc/zs/SlaveSynStatusMain.java
@@ -7,22 +7,22 @@ public class SlaveSynStatusMain {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// 20 主库
 		System.out.println("-------------22--------------");
-		//JDBCSimple22.doIt(); // 从库
-		
+		// JDBCSimple22.doIt(); // 从库
+
 		System.out.println("-------------224--------------");
-		//JDBCSimple224.doIt(); // 从库
-		
+		// JDBCSimple224.doIt(); // 从库
+
 		System.out.println("-------------26--------------");
-		//JDBCSimple26.doIt(); // 从库
-		
+		// JDBCSimple26.doIt(); // 从库
+
 		System.out.println("-------------224 3307--------------");
 		JDBCSimple2243307.doIt(); // 备主
-		
+
 		System.out.println("-------------26 3307--------------");
-		//JDBCSimple263307.doIt(); // 备从
+		// JDBCSimple263307.doIt(); // 备从
 
 		System.out.println("-------------26 3308--------------");
 		// dw主192.168.10.224:3308
-		//JDBCSimple263308.doIt(); // dw从192.168.10.26:3308
+		// JDBCSimple263308.doIt(); // dw从192.168.10.26:3308
 	}
 }
