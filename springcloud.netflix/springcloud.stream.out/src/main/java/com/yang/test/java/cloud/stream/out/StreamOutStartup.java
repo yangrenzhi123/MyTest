c44aea20cf090fae046976ca936c024bbbcb823d
yangrenzhi123 <yangrenzhi123@163.com>
2020-08-28 16:13:53 +0800

    1

---
+++ springcloud.netflix/springcloud.stream.out/src/main/java/com/yang/test/java/cloud/stream/out/StreamOutStartup.java
@@ -0,0 +1,12 @@
+package com.yang.test.java.cloud.stream.out;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamOutStartup {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamOutStartup.class);
+    }
+}
