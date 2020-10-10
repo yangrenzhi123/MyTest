3f63cfc2be8dffbf4b33a2ca6b394aa981b86825
yangrenzhi123 <yangrenzhi123@163.com>
2020-10-10 09:55:13 +0800

    1

--- springboot/src/main/java/com/yang/test/java/springboot/Springboot.java
+++ springboot/src/main/java/com/yang/test/java/springboot/Springboot.java
@@ -1,30 +1,25 @@
 package com.yang.test.java.springboot;
 
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.yang.test.java.springboot.entity.Man;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Springboot {
 
-	@Bean
-	public Man test() throws ClassNotFoundException, IOException {
-		URL[] urls = new URL[] {new URL("file:/c:/classes/")};
-		URLClassLoader cl = new URLClassLoader(urls);
-		Class<?> c = cl.loadClass("com.yang.test.java.springboot.controller.TestController");
-		System.out.println(c.getPackage());
-		cl.close();
-		
-		return new Man();
-	}
-
 	public static void main(String[] args) throws IOException, InterruptedException {
 		SpringApplication.run(Springboot.class, args);
+	}
+}
+
+@RestController
+class TestController {
+
+	@RequestMapping("/")
+	public String index() throws Exception {
+		return "success";
 	}
 }
