1f5edc71ee9e1573f3040dd76b4555578c3c9908
yangrenzhi123 <yangrenzhi123@163.com>
2021-02-27 23:10:05 +0800

    1

--- com.yang.test.java/src/main/java/ThreadLocalUtils.java
+++ com.yang.test.java/src/main/java/ThreadLocalUtils.java
@@ -1,11 +1,18 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.alibaba.ttl.TransmittableThreadLocal;
 
 public class ThreadLocalUtils {
 
 	public static final ThreadLocal<DateFormat> yyyyMMddHHmmss = new ThreadLocal<>();
 	public static final InheritableThreadLocal<String> test = new InheritableThreadLocal<>();
 	public static final ThreadLocal<String> test2 = new ThreadLocal<>();
+	public static final ThreadLocal<String> test3 = new TransmittableThreadLocal<>();
+	
+	public static final ExecutorService p = Executors.newFixedThreadPool(10);
 
 	public static DateFormat getyyyyMMddHHmmss() {
 		DateFormat df = yyyyMMddHHmmss.get();
@@ -17,17 +24,28 @@ public class ThreadLocalUtils {
 	}
 
 	public static void main(String[] args) {
-		test2.set("ok");
+		test3.set("ok");
 
 
+		// 线程初始的时候，会从父线程copy线程变量的地址引用。
 		new Thread(new Runnable() {
 			public void run() {
-				new Thread(new Runnable() {
-					public void run() {
-						System.out.println(test2.get());
-					}
-				}).start();
+				System.out.println(test3.get());
 			}
 		}).start();
+		
+		// 数据发生修改，子线程里面的数据不会修改。
+		test3.set("ok-2");
+		System.out.println();
+
+		for (int i = 0; i < 10; i++) {
+			p.execute(new Runnable() {
+				public void run() {
+					System.out.println(test3.get());
+				}
+			});
+		}
+		
+		p.shutdown();
 	}
 }
