4fbfa4a3fa168081b83118140feb23a789a063f1
yrz <yangrenzhi123@163.com>
2021-03-10 16:02:22 +0800

    1

--- springboot.websocket/src/main/java/com/yang/test/java/springboot/controller/TestController.java
+++ springboot.websocket/src/main/java/com/yang/test/java/springboot/controller/TestController.java
@@ -1,10 +1,16 @@
 package com.yang.test.java.springboot.controller;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.websocket.Session;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,18 +26,6 @@ public class TestController {
 	WebSocketServer webSocketServer;
 
 	/**
-	 * 群发消息内容
-	 *
-	 * @param message
-	 * @return
-	 */
-	@RequestMapping(value = "/ws/sendAll", method = RequestMethod.GET)
-	public String sendAllMessage(@RequestParam(required = true) String message) throws IOException {
-		webSocketServer.broadCastInfo(message);
-		return "success";
-	}
-
-	/**
 	 * 指定会话ID发消息
 	 *
 	 * @param message 消息内容
@@ -42,5 +36,21 @@ public class TestController {
 	public String sendOneMessage(@RequestParam(required = true) String message, @RequestParam(required = true) String userId) throws IOException {
 		webSocketServer.sendToUser(userId, message);
 		return "success";
+	}
+
+	@GetMapping(value = "/")
+	public void all() throws IOException {
+
+		Iterator<Map.Entry<String, List<Session>>> iter = WebSocketServer.sessions.entrySet().iterator();
+		while (iter.hasNext()) {
+			Map.Entry<String, List<Session>> entry = (Map.Entry<String, List<Session>>) iter.next();
+			String key = (String)entry.getKey();
+			List<Session> l = (List<Session>) entry.getValue();
+			
+			System.out.println("userId：" + key);
+			for(Session s : l) {
+				System.out.println(s);
+			}
+		}
 	}
 }
